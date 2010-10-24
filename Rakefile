307753deba914b7746f80a6ac02b4515f5986cff
Dominik Menke <dmke@tzi.de>
2010-10-25 01:43:35 +0200

    switched from sefmade-rack to sinatra + sequel.

--- Rakefile
+++ Rakefile
@@ -1,31 +1,12 @@
 CC_PID_FILE = File.join(File.dirname(__FILE__), 'command-control', 'cc.pid')
 
-def call_rake(task, rakefile = __FILE__)
-  system "/usr/bin/rake #{task} -s -f #{rakefile} 2>&1 >> #{File.join(File.dirname(__FILE__), 'log', "rake-#{task}.log")} &"
-end
-
 ### defs  above ###
 ### tasks below ###
 
-desc "Default task. Starts a CC server and does other things..."
+desc "Default task. Remembers you to start the command&control server"
 task :default => [CC_PID_FILE]
 
 file CC_PID_FILE do
-  call_rake :start_cc
-end
-
-desc "Starts the CC server."
-task :start_cc do
-  require './command-control/cc.rb'
-  CommandControl.boot  
-end
-
-desc "Stops the CC server"
-task :stop_cc do
-  if File.exists? CC_PID_FILE
-    pid = File.read(CC_PID_FILE).strip
-    sh "kill -15 #{pid}" if pid =~ /\d+/
-    FileUtils.rm CC_PID_FILE
-  end
+  raise "Start your command&control server!"
 end
 
--- command-control/cc.rb
+++ command-control/cc.rb
@@ -1,30 +1,53 @@
 #!/usr/bin/env ruby
 
-require 'thin'
-require 'rack'
-require 'yaml'
-require 'fileutils'
-require 'pp' # pretty print things, e.g. `pp ENV`
+require 'rubygems'
+require 'sinatra'
+require 'sequel'
 
-require File.join(File.dirname(__FILE__), 'lib', 'cc-interface.rb')
-require File.join(File.dirname(__FILE__), 'lib', 'cc-servlet.rb')
-
-module CommandControl
-  PID_FILE = File.join(File.dirname(__FILE__), 'cc.pid')
-  def boot
-    config = YAML.load_file File.join(File.dirname(__FILE__), 'cc.yaml')
-    puts "Starting #{CommandControl::Servlet.signature}"
-    if File.exists? PID_FILE
-      puts "There is aready an instance running (pid #{File.read(PID_FILE).strip})"
-      return
-    end
-    puts "Process id: #{Process.pid}"
-    File.open(PID_FILE, 'w+') do |f|
-      f.write Process.pid
-    end
-    Thin::Server.start '0.0.0.0', config["port"], CommandControl::Servlet.new
-    FileUtils.rm_f PID_FILE
+DB = Sequel.sqlite 'cc.db'
+unless DB.table_exists? :bots
+  DB.create_table :bots do
+    primary_key :id
+    String :hostname
+    String :ip
+    DateTime :last_seen
   end
-  module_function :boot
 end
 
+enable :run
+set :port => 80
+
+def header title = 'FIDIUS CC Server version 0.1.0'
+  "<html><head><title>#{title}</title></head><body><h1>#{title}</h1>"
+end
+
+def footer
+  "</body></html>"
+end
+
+get '/info' do
+  body  = header
+  body << '<table>'
+  body << '<tr><th>ID</th><th>Hostname</th><th>IP</th><th>seen</th></tr>'
+  if DB[:bots].count > 0
+    DB[:bots].each do |bot|
+      body << '<tr>'
+      body << "<td>#{bot[:id]}</td>"
+      body << "<td>#{bot[:hostname]}</td>"
+      body << "<td>#{bot[:ip]}</td>"
+      body << "<td>#{bot[:last_seen]}</td>"
+      body << '</tr>'
+    end
+  else
+    body << '<tr><td colspan="5">No database entries.</td></tr>'
+  end
+  body << '</table>'
+  body << footer
+end
+
+get '/session' do  
+  id = DB[:bots].insert(:hostname => '--', :ip => request.ip, :last_seen => DateTime.now)
+  body  = header
+  body << "Your session id is <strong>#{id}</strong>."
+  body << footer
+end
