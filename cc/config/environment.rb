c69dfdbecd32938585c1da20a0f0074c8a1d0950
Dominik Menke <dmke@tzi.de>
2010-11-02 16:54:17 +0100

    integrated msf models

--- cc/config/environment.rb
+++ cc/config/environment.rb
@@ -1,5 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+yaml = YAML::load(open(File.join(File.dirname(__FILE__), 'msf.yml')))
+LOCAL_MSF_SVN_CHECKOUT = yaml['msf_lib']
+
+Dir.new(File.join(LOCAL_MSF_SVN_CHECKOUT, 'lib', 'msf', 'core')) do
+  require File.join(LOCAL_MSF_SVN_CHECKOUT, 'lib', 'msf', 'core', 'db.rb')
+  Dir.glob(File.join(LOCAL_MSF_SVN_CHECKOUT, 'lib', 'msf', 'core', 'model', '*.rb')).each do |rb|
+    require rb
+  end
+end
+
 # Initialize the rails application
 Cc::Application.initialize!
