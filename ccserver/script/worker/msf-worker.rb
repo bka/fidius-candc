227d755af6c7955b0fc10953ec09a1c1025e580f
Marten Wirsik <mwirsik@tzi.de>
2010-11-04 17:36:16 +0100

    BugFix von lokalem Profil

--- ccserver/script/worker/msf-worker.rb
+++ ccserver/script/worker/msf-worker.rb
@@ -1,17 +1,18 @@
 require "yaml"
 require 'rubygems'
 require 'eventmachine'
-require "/Users/mwirsik/Studium/Bremen/FIDIUS/git/msf-autopwn/lib/subnet_manager.rb"
+
 
 puts "Loading MsfConfig"
-raise "could not load configuration file" unless (MSF_PATH = YAML::parse_file "#{RAILS_ROOT}/config/msf.yml")
+raise "could not load configuration file" unless (MSF_SETTINGS = YAML::parse_file "#{RAILS_ROOT}/config/msf.yml")
 puts "Loading DataBase Config"
 raise "could not load database configuration file" unless (DB_SETTINGS = YAML.load_file("#{RAILS_ROOT}/config/database.yml")[RAILS_ENV])
 
-PATH_TO_MSF_LIB="#{MSF_PATH.select("/msf_lib").first.value}"
+PATH_TO_MSF_LIB="#{MSF_SETTINGS.select("/msf_lib").first.value}"
 
 $:.unshift(PATH_TO_MSF_LIB)
-
+puts MSF_SETTINGS.select("/subnet_manager_path").first.value
+require "#{MSF_SETTINGS.select("/subnet_manager_path").first.value}"
 require "msf/base"
 # require "rex"
 
