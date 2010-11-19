31e34823879ad22bab27c02cfe6e5981ae7ea6b1
Hauke Mehrtens <hauke@hauke-m.de>
2010-11-19 17:52:30 +0100

    drb: connect only if needed and try to store the connection
    see http://stackoverflow.com/questions/299219/where-is-the-correct-place-to-initialize-the-drb-service-within-a-rails-applicati

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,22 +2,30 @@ require 'drb'
 
 class TasksController < ApplicationController
 
-  def initialize
+  def get_msf_worker
     msf = YAML::load(open(File.join(File.dirname(__FILE__), '..', '..', 'config', 'msf.yml')))
     if not msf['drb_url']
       puts "please specify drb_url in msf.yml"
       return
     end
 
-    DRb.start_service
-    @msf_worker = DRbObject.new nil, msf['drb_url']
+    # start DRb service if it hasn't been started before
+    begin
+        DRb.current_server
+    rescue DRb::DRbServerNotFound
+        DRb.start_service
+        # move to different ThreadGroup to avoid mongrel hang on exit
+        ThreadGroup.new.add DRb.thread
+    end
+    DRbObject.new nil, msf['drb_url']
   end
+
   def index
 
   end
 
   def scan
-    @msf_worker.autopwn params[:subnet]
+    get_msf_worker.autopwn params[:subnet]
     flash[:notice] = "Scan started"
     redirect_to :root
   end
