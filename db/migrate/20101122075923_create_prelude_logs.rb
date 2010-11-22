2c960cd10aefaa8c17bdb3f4a0c8e62145cd032b
bernd <B.Katzmarski@gmx.de>
2010-11-22 22:30:25 +0100

    oops forgot migration

---
+++ db/migrate/20101122075923_create_prelude_logs.rb
@@ -0,0 +1,20 @@
+class CreatePreludeLogs < ActiveRecord::Migration
+  def self.up
+    create_table :prelude_logs do |t|
+      t.integer :task_id
+      t.column :payload, :longblob
+      t.datetime :detect_time
+      t.string :dest_ip
+      t.string :src_ip
+      t.string :text
+      t.string :severity
+      t.string :analyzer_model
+      t.column :ident, :bigint
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :prelude_logs
+  end
+end
