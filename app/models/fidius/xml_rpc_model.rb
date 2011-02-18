491c2b03c18a28d4d6e6c793f57a26ddd9e3e40a
bernd <B.Katzmarski@gmx.de>
2011-02-18 11:21:11 +0100

    work in progress

--- app/models/fidius/xml_rpc_model.rb
+++ app/models/fidius/xml_rpc_model.rb
@@ -17,45 +17,63 @@ class FIDIUS::XmlRpcModel < ActiveRecord::Base
     return XMLRPC::Client.new(host,"/",port)
   end
 
-  def self.find_by_sql(query)
-
+  def to_a
+    puts "to_a"
   end
 
   def self.first
-
+    find(:first)
   end
 
   def self.last
-
+    find(:last)
   end
 
   def self.find(*args)
+    puts "find #{args.inspect}"
     # TODO: find :all
     # TODO: more generic, not only hosts
-    rpc = self.connect
     model_name = self.name
-    begin
-      xml = rpc.call("model.host.find",args)
-        doc=REXML::Document.new(xml)
-        doc.root.each_element('//host') do |tag|
-          object = nil
-          eval("object = #{model_name}.new")
-          tag.each_element do |e|
-            key = e.name
-            value = e.children.first
-            eval("object.#{key} = #{value}")
-          end
-          puts object.inspect
-        end
-    rescue XMLRPC::FaultException=>e
-      puts "ERROR: *"
-      puts "CODE: #{e.faultCode}"
-      puts "FAULT: #{e.faultString}"
-    end
-    nil
+    call_rpc "model.#{model_name.downcase}.find",*args
   end
   
   def self.all(*args)
     find(:all, *args)
+  end
+
+  def self.call_rpc(method, *args)
+    begin
+      rpc = self.connect
+      puts "parse_xml #{method} #{args.inspect}"
+      return parse_xml rpc.call(method,args)
+    rescue XMLRPC::FaultException=>e
+      raise "#{e.faultString}(#{e.faultCode})"
+      #puts "ERROR: *"
+      #puts "CODE: #{e.faultCode}"
+      #puts "FAULT: #{e.faultString}"
+    end
+    nil
+  end
+
+  def self.parse_xml(xml)
+
+    res = Array.new
+    doc=REXML::Document.new(xml)
+    doc.root.each_element('//host | //fidius-asset-host') do |tag|
+      object = nil
+      eval("object = #{model_name}.new")
+      tag.each_element do |e|
+        key = e.name
+        value = e.children.first
+        if value
+          eval("object.#{key} = #{value}")
+        else
+          eval("object.#{key} = nil")
+        end
+      end
+      res << object
+      puts object.inspect
+    end
+    return res
   end
 end
