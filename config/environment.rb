e9b897c8b6f19743d199a5494443bf3021b109bb
Dominik Menke <dmke@tzi.de>
2011-03-18 02:31:57 +0100

    moved redundant code into seperate gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 CommandAndControl::Application.initialize!
-
-# unfortunately json-addon has to be loaded after activesupport
-require File.join("#{::Rails.root.to_s}","config","initializers","json_symbol_addon.rb")
