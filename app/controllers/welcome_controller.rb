df1ffc65dbc686824b7ed033004b782ec34c41e0
Andreas Bender <bender@informatik.uni-bremen.de>
2011-07-08 13:36:03 +0200

    controller vergessen

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,24 @@
+class WelcomeController < ApplicationController
+  
+  def credits
+    t = render_to_string 'credits', :layout => false
+    render :update do |page|
+      page <<%{
+        $('#standard_dialog').html("#{escape_javascript(t)}");
+      }
+    end    
+  end
+  
+  def documentation
+    t = render_to_string 'documentation', :layout => false
+    render :update do |page|
+      page <<%{
+        $('#standard_dialog').html("#{escape_javascript(t)}");
+      }
+    end 
+  end
+  
+  def license
+  end
+
+end
